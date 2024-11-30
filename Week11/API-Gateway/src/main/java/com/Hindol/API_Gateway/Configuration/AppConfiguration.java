492ea3612fbec28e1c2319c7a7a32628f2618e4b
hRJ06 <hindolroy.2306@gmail.com>
2024-12-01 00:33:38 +0530

    feat: add Zipkin and Micrometer configuration for distributed tracing

--- Week11/API-Gateway/src/main/java/com/Hindol/API_Gateway/Configuration/AppConfiguration.java
+++ Week11/API-Gateway/src/main/java/com/Hindol/API_Gateway/Configuration/AppConfiguration.java
@@ -1,18 +1,13 @@
-package com.Hindol.Order.Configuration;
+package com.Hindol.API_Gateway.Configuration;
 
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
-    @Bean
-    public ModelMapper getModelMapper() {
-        return new ModelMapper();
-    }
     @Bean
     public Capability capability(final MeterRegistry registry) {
         return new MicrometerCapability(registry);
--- Week11/Inventory/src/main/java/com/Hindol/Inventory/Configuration/AppConfiguration.java
+++ Week11/Inventory/src/main/java/com/Hindol/Inventory/Configuration/AppConfiguration.java
@@ -1,5 +1,8 @@
 package com.Hindol.Inventory.Configuration;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +17,9 @@ public class AppConfiguration {
     @Bean
     public RestClient getRestClient() {
         return RestClient.builder().build();
+    }
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
     }
 }
