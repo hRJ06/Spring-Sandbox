a3a5176a1cd2364ea029214693f3636653f6c0a4
hRJ06 <hindolroy.2306@gmail.com>
2024-12-29 19:43:36 +0530

    chore: Add k8s configuration for ingress setup of API gateway and zipkin, fix DB configuration

--- Week11, Week12/Inventory/src/main/java/com/Hindol/Inventory/Client/OrdersOpenFeignClient.java
+++ Week11, Week12/Inventory/src/main/java/com/Hindol/Inventory/Client/OrdersOpenFeignClient.java
@@ -3,7 +3,7 @@ package com.Hindol.Inventory.Client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "Order-Service", path = "/orders")
+@FeignClient(name = "Order-Service", path = "/orders", url = "${ORDER_SERVICE_URI:}")
 public interface OrdersOpenFeignClient {
     @GetMapping("/core/hello")
     String helloOrders();
--- Week11, Week12/Order/src/main/java/com/Hindol/Order/Client/InventoryOpenFeignClient.java
+++ Week11, Week12/Order/src/main/java/com/Hindol/Order/Client/InventoryOpenFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Inventory-Service", path = "/inventory")
+@FeignClient(name = "Inventory-Service", path = "/inventory", url = "${INVENTORY_SERVICE_URI:}")
 public interface InventoryOpenFeignClient {
     @PutMapping("/products/reduce-stocks")
     Double reduceStocks(@RequestBody OrderRequestDTO orderRequestDTO);
